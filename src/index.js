e388bc94e93dfe6888789222eca3d33b19f55869
Evgeniy <yevgeniykyr5@ukr.netgit config --global user.name prostoevgeniy5git config --global user.email yevgeniykyr5@ukr.net>
2021-03-07 18:30:09 +0200

    feat: created functions min(arr), max(arr), avg(arr)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+
+  if(typeof array === undefined || array.length === 0) {
+      return 0;
+    }
+
+    let result = array.reduce((ac, el, ind) => {
+      if(ac > el) {
+          return el;
+      }
+      
+      return ac;  
+    }, array[1])
+
+  return result;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+ 
+  if(typeof array === undefined || array.length === 0) {
+      return 0;
+    }
+    let result = array.reduce((ac, el, ind) => {
+      if(ac < el) {
+          return el;
+      }
+
+      return ac;
+    }, array[1])
+
+  return result;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  let result;
+  if(typeof array === undefined || array.length === 0) {
+    return 0;
+  }
+
+  result = array.reduce((ac, el, ind) => {
+    return ac + el;
+  }, 0)
+
+  return result / array.length;
 }
